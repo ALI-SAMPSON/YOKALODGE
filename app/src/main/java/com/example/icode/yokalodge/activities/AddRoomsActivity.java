4d164b0fdf694adaa6dbd189b99e6ddee1e3d93c
iCODE <alisampson2@gmail.com>
2018-07-25 10:09:13 -0700

    initial commit

---
+++ app/src/main/java/com/example/icode/yokalodge/activities/AddRoomsActivity.java
@@ -0,0 +1,15 @@
+package com.example.icode.yokalodge.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.icode.yokalodge.R;
+
+public class AddRoomsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_rooms);
+    }
+}
